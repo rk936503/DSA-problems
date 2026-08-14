52c1faa41108328b2cf9e650671e4fdd2407aa1b
Rahul Kumar <149233745+rk936503@users.noreply.github.com>
2026-08-14 20:50:36 +0530

    Time: 19 ms (75.99%), Space: 77.6 MB (61.87%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -6,19 +6,23 @@ public:
         int maxLen = 0;
         int l=0, r=0;
         for(int r=0; r<nums.size(); r++){
+            //maintaining decreasing deque for max num from front
             while(!maxDq.empty() && maxDq.back()<nums[r]){
                 maxDq.pop_back();
             }
+            //maintaining increasing deque for min num from front
             maxDq.push_back(nums[r]);
             while(!minDq.empty() && minDq.back()>nums[r]){
                 minDq.pop_back();
             }
             minDq.push_back(nums[r]);
+            //if limit exceeds, shrink window
             while(maxDq.front() - minDq.front() > limit){
                 if(nums[l] == maxDq.front()) maxDq.pop_front();
                 if(nums[l] == minDq.front()) minDq.pop_front();
                 l++;
             }
+            //current window
             maxLen = max(maxLen, r-l+1);
         }
 
